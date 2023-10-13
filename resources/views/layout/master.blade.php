31d33b79b33592b554917eb206293ec225034a05
Diwas KC <diwaskc12@gmail.com>
2023-10-13 12:28:12 +0545

    commit latest changes

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -11,7 +11,6 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
         <!-- Styles -->
-        @viteReactRefresh
    
         @vite('resources/css/app.css') 
         @vite('resources/js/app.jsx') 
